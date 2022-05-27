a26b794e50466839ceffef3f07742a935e23b50d
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-05-27 09:23:51 +0100

    Base Retrofit

---
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21904825_21904341/FireService.kt
@@ -0,0 +1,4 @@
+package pt.ulusofona.deisi.cm2122.g21904825_21904341
+
+interface FireService {
+}
---
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21904825_21904341/RetrofitBuilder.kt
@@ -0,0 +1,17 @@
+package pt.ulusofona.deisi.cm2122.g21904825_21904341
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitBuilder {
+
+    fun getInstance(path: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(path)
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
