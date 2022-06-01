7fafd4d4747ebce15111c89c62f8559a6a799ac2
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-06-01 22:46:58 +0100

    Filter in ListFragment.kt

--- app/src/main/java/pt/ulusofona/deisi/cm2122/g21904825_21904341/ListAdapter.kt
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21904825_21904341/ListAdapter.kt
@@ -2,13 +2,17 @@ package pt.ulusofona.deisi.cm2122.g21904825_21904341
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import pt.ulusofona.deisi.cm2122.g21904825_21904341.databinding.ItemExpressionBinding
 
 class ListAdapter (private val onFireClick: (Fire) -> Unit,
-                   private var items: List<Fire> = listOf()) : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+                   private var items: List<Fire> = listOf()) : RecyclerView.Adapter<ListAdapter.ListViewHolder>(), Filterable {
 
     class ListViewHolder(val binding: ItemExpressionBinding) : RecyclerView.ViewHolder(binding.root)
+
+    var untouchedList = arrayListOf<Fire>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         return ListViewHolder(
@@ -34,6 +38,34 @@ class ListAdapter (private val onFireClick: (Fire) -> Unit,
 
     fun updateItems(items: List<Fire>) {
         this.items = items
+        this.untouchedList = items as ArrayList<Fire>
         notifyDataSetChanged()
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charSearch = constraint.toString()
+                if(charSearch.isEmpty()) {
+                    items = untouchedList
+                } else {
+                    val resultList = ArrayList<Fire>()
+                    for (fire in items) {
+                        if(fire.getDistrict().lowercase().contains(constraint.toString().lowercase())) {
+                            resultList.add(fire)
+                        }
+                    }
+                    items = resultList
+                }
+                val filteredResults = FilterResults()
+                filteredResults.values = items
+                return filteredResults
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                items = results?.values as ArrayList<Fire>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
--- app/src/main/java/pt/ulusofona/deisi/cm2122/g21904825_21904341/ListFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21904825_21904341/ListFragment.kt
@@ -3,16 +3,16 @@ package pt.ulusofona.deisi.cm2122.g21904825_21904341
 import android.content.Context
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import pt.ulusofona.deisi.cm2122.g21904825_21904341.databinding.FragmentListBinding
+
 
 class ListFragment : Fragment() {
     private lateinit var binding: FragmentListBinding
@@ -42,6 +42,7 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
         binding = FragmentListBinding.bind(view)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
@@ -58,5 +59,32 @@ class ListFragment : Fragment() {
             adapter.updateItems(fires)
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+        val menuItem = menu.findItem(R.id.action_search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                adapter.filter.filter(newText)
+                return true
+            }
+        })
+        menuItem.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                adapter.filter.filter("")
+                return true
+            }
+
+            override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+                return true
+            }
+        })
+        return super.onCreateOptionsMenu(menu, inflater)
     }
 }
