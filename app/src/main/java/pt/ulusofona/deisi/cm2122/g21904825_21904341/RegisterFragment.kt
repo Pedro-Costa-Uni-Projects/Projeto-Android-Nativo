ead971a14ed55d74745d712e37672fb773179061
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-06-01 16:07:37 +0100

    clean imports

--- app/src/main/java/pt/ulusofona/deisi/cm2122/g21904825_21904341/RegisterFragment.kt
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g21904825_21904341/RegisterFragment.kt
@@ -9,12 +9,9 @@ import android.graphics.BitmapFactory
 import android.location.Geocoder
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
